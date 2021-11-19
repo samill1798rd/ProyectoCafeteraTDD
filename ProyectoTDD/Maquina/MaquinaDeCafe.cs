41bd083dc03e60830f0b360036e09a2fdfd6ad67
Samill <samill.rivasrd@gmail.com>
2021-11-18 23:41:50 -0400

    Implementacion de todos los test de la maquina

--- ProyectoTDD/Maquina/MaquinaDeCafe.cs
+++ ProyectoTDD/Maquina/MaquinaDeCafe.cs
@@ -7,20 +7,92 @@ namespace ProyectoTDD.Maquina
     public class MaquinaDeCafe
     {
         private Cafetera Cafe { get; set; }
-        private Vaso vasosPequenos { get; set; }
-        private Vaso vasosMedianos { get; set; }
-        private Vaso vasosGrande { get; set; }
+        public Vaso vasosPequenos { get; set; }
+        public Vaso vasosMedianos { get; set; }
+        public Vaso vasosGrandes { get; set; }
         private Azucarero azucar { get; set; }
 
-        public Vaso getTipoVaso(int tipoDeVaso)
+        public Vaso getTipoVaso(string tipoVaso)
         {
-            return null;
+            Vaso vasoNew = null;
+            switch (tipoVaso)
+            {
+                case "pequeno":
+                    vasoNew = vasosPequenos;
+                    break;
+                case "mediano":
+                    vasoNew = vasosMedianos;
+                    break;
+                case "grande":
+                    vasoNew = vasosGrandes;
+                    break;
+            }
+
+            return vasoNew;
         }
 
-        public Vaso getVasoDeCafe(int tipoDeVaso,int cantidadDeVaso, int cantidadDeAzucar)
+        public string getVasoDeCafe(Vaso tipoDeVaso, int cantidadDeVaso, int cantidadDeAzucar)
         {
-            return null;
+            if (!tipoDeVaso.hasVasos(cantidadDeVaso))
+                return "no hay vasos";
+            if (!this.Cafe.hasCafe(tipoDeVaso.getContenido()))
+                return "no hay cafe";
+            if (!this.azucar.hasAzucar(cantidadDeAzucar))
+                return "no hay azucar";
+       
+            if (this.vasosPequenos.getContenido() == tipoDeVaso.getContenido())
+                this.vasosPequenos.giveVasos(cantidadDeVaso);
+            else if (this.vasosMedianos.getContenido() == tipoDeVaso.getContenido())
+                this.vasosMedianos.giveVasos(cantidadDeVaso);
+            else if (this.vasosGrandes.getContenido() == tipoDeVaso.getContenido())
+                this.vasosGrandes.giveVasos(cantidadDeVaso);
+            else return "Error al Obtener Vaso";
+
+            this.azucar.giveAzucar(cantidadDeAzucar);
+            this.Cafe.giveCafe(tipoDeVaso.getContenido());
+            return "Felicitaciones";
         }
+
+        public void setVasosPequeno(Vaso VasosPequeno)
+        {
+            vasosPequenos = VasosPequeno;
+        }
+        public void setVasosMediano(Vaso VasosMediano)
+        {
+            vasosMedianos = VasosMediano;
+        }
+
+        public void setVasosGrande(Vaso VasosGrande)
+        {
+            vasosGrandes = VasosGrande;
+        }
+
+        public void setAzucarero(Azucarero azucarero)
+        {
+            azucar = azucarero;
+        }
+
+        public void setCafetera(Cafetera cafetera)
+        {
+            Cafe = cafetera;
+        }
+
+        public Cafetera getCafetera()
+        {
+            return Cafe;
+        }
+
+        public Vaso getVasosPequeno()
+        {
+            return vasosPequenos;
+        }
+
+        public Azucarero getAzucarero()
+        {
+            return azucar;
+        }
+
+
 
 
     }
---
+++ Test/TestMaquinaDeCafe.cs
@@ -0,0 +1,164 @@
+﻿using ProyectoTDD.Maquina;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test
+{
+    public class TestMaquinaDeCafe
+    {
+        public Cafetera cafetera;
+        public Vaso vasosPequenos;
+        public Vaso vasosMediano;
+        public Vaso vasosGrande;
+        public Azucarero azucarero;
+        public MaquinaDeCafe maquinaDeCafe;
+        public TestMaquinaDeCafe()
+        {
+            cafetera = new Cafetera(50);
+            vasosPequenos = new Vaso(5,10);
+            vasosMediano = new Vaso(5,20);
+            vasosGrande = new Vaso(5,30);
+            azucarero = new Azucarero(20);
+
+            maquinaDeCafe = new MaquinaDeCafe();
+            maquinaDeCafe.setVasosPequeno(vasosPequenos);
+            maquinaDeCafe.setVasosMediano(vasosMediano);
+            maquinaDeCafe.setVasosGrande(vasosGrande);
+            maquinaDeCafe.setAzucarero(azucarero);
+        }
+
+        [Fact]
+        public void deberiaDevolverUnVasoPequeno()
+        {
+            var vaso = maquinaDeCafe.getTipoVaso("pequeno");
+
+            Assert.Equal(maquinaDeCafe.vasosPequenos, vaso);
+        }
+
+        [Fact]
+        public void deberiaDevolverUnVasoMediano()
+        {
+            var vaso = maquinaDeCafe.getTipoVaso("mediano");
+
+            Assert.Equal(maquinaDeCafe.vasosMedianos, vaso);
+        }
+
+        [Fact]
+        public void deberiaDevolverUnVasoGrande()
+        {
+            var vaso = maquinaDeCafe.getTipoVaso("grande");
+
+            Assert.Equal(maquinaDeCafe.vasosGrandes, vaso);
+        }
+
+        [Fact]
+        public void deberiaDevolverNoHayVasos()
+        {
+            var vaso = maquinaDeCafe.getTipoVaso("pequeno");
+
+            var result = maquinaDeCafe.getVasoDeCafe(vaso, 10, 2);
+
+            Assert.Equal("no hay vasos",result);
+        }
+
+        [Fact]
+        public void deberiaDevolverNoHayCafe()
+        {
+            cafetera = new Cafetera(5);
+            maquinaDeCafe.setCafetera(cafetera);
+
+            var vaso = maquinaDeCafe.getTipoVaso("pequeno");
+
+            var result = maquinaDeCafe.getVasoDeCafe(vaso, 1, 2);
+
+            Assert.Equal("no hay cafe", result);
+        }
+
+        [Fact]
+        public void deberiaDevolverNoHayAzucar()
+        {
+            cafetera = new Cafetera(10);
+            maquinaDeCafe.setCafetera(cafetera);
+            azucarero = new Azucarero(2);
+            maquinaDeCafe.setAzucarero(azucarero);
+
+            var vaso = maquinaDeCafe.getTipoVaso("pequeno");
+
+            var result = maquinaDeCafe.getVasoDeCafe(vaso, 1, 3);
+
+            Assert.Equal("no hay azucar", result);
+        }
+
+        [Fact]
+        public void deberiaRestarCafe()
+        {
+            cafetera = new Cafetera(50);
+            maquinaDeCafe.setCafetera(cafetera);
+            azucarero = new Azucarero(20);
+            maquinaDeCafe.setAzucarero(azucarero);
+
+
+            var vaso = maquinaDeCafe.getTipoVaso("pequeno");
+
+            maquinaDeCafe.getVasoDeCafe(vaso, 1, 3);
+
+            var result = maquinaDeCafe.getCafetera().getCantidadDeCafe();
+
+            Assert.Equal(40, result);
+        }
+
+        [Fact]
+        public void deberiaRestarVaso()
+        {
+            cafetera = new Cafetera(50);
+            maquinaDeCafe.setCafetera(cafetera);
+            azucarero = new Azucarero(20);
+            maquinaDeCafe.setAzucarero(azucarero);
+
+
+            var vaso = maquinaDeCafe.getTipoVaso("pequeno");
+
+            maquinaDeCafe.getVasoDeCafe(vaso, 1, 3);
+
+            var result = maquinaDeCafe.getVasosPequeno().getCantidadVasos();
+
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void deberiaRestarAzucar()
+        {
+            cafetera = new Cafetera(50);
+            maquinaDeCafe.setCafetera(cafetera);
+            azucarero = new Azucarero(20);
+            maquinaDeCafe.setAzucarero(azucarero);
+
+
+            var vaso = maquinaDeCafe.getTipoVaso("pequeno");
+
+            maquinaDeCafe.getVasoDeCafe(vaso, 1, 3);
+
+            var result = maquinaDeCafe.getAzucarero().getCantidadDeAzucar();
+
+            Assert.Equal(17, result);
+        }
+
+        [Fact]
+        public void deberiaDevolverFelicitaciones()
+        {
+            cafetera = new Cafetera(50);
+            maquinaDeCafe.setCafetera(cafetera);
+            azucarero = new Azucarero(20);
+            maquinaDeCafe.setAzucarero(azucarero);
+
+            var vaso = maquinaDeCafe.getTipoVaso("pequeno");
+
+            var result = maquinaDeCafe.getVasoDeCafe(vaso, 1, 3);
+
+  
+            Assert.Equal("Felicitaciones",result);
+        }
+    }
+}
