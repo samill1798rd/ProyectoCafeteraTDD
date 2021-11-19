42cfd269ac58df1e864696ebc528ffdb52aabf5c
Samill <samill.rivasrd@gmail.com>
2021-11-18 20:52:47 -0400

    Add project files.

---
+++ ProyectoTDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
